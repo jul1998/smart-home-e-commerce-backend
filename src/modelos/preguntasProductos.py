72c14c055309e7093007797337ca1da73c96f04b
jul1998 <jguevara321@gmail.com>
2022-11-18 22:56:08 +0000

    preguntasModel

--- src/modelos/preguntasProductos.py
+++ src/modelos/preguntasProductos.py
@@ -5,9 +5,10 @@ import os
 class PreguntasProductos(db.Model):
     __tablename__ = "preguntasproductos"
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ask_by_userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     productId = db.Column(db.Integer, db.ForeignKey('producto.id'))
     descripcion = db.Column(db.String(2000))
+    posted_at = db.Column(db.DateTime, nullable=False)
     estado = db.Column(db.String(60), nullable=False)
 
 
