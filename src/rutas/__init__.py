0b619949be070705470e7e40c92128a04d330c23
jul1998 <jguevara321@gmail.com>
2022-11-24 03:37:12 +0000

    added singupadmin to init

--- src/rutas/__init__.py
+++ src/rutas/__init__.py
@@ -2,4 +2,5 @@ from .user import signup
 from .product import create_product, get_product_list, add_to_favorite_list
 from .historial_compras import get_order_history
 from .producto_preguntas import post_product_question, get_questions
+from .userAdmin import signupAmin
   
