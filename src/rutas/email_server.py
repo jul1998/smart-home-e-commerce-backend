4550b5252002a6f0354a8aa6fd58c1d25d0151d8
jul1998 <jguevara321@gmail.com>
2022-11-26 07:47:13 +0000

    fixed bugs in email

--- src/rutas/email_server.py
+++ src/rutas/email_server.py
@@ -12,6 +12,7 @@ PASSWORD = os.environ.get("PASSWORD")
 @app.route("/email", methods=["POST", "GET"])
 def send_email():
     body = request.get_json()
+    smtp_port = 465  # SSL
 
     msg_body = body["msg_body"]
     recipient = body["recipient"]
@@ -19,14 +20,36 @@ def send_email():
     print(msg_body, recipient, subject)
     print(EMAIL, PASSWORD)
 
+    # em = EmailMessage()
+    # em["From"] = EMAIL
+    # em["To"] = recipient
+    # em["Subject"] = subject
+    # em.set_content(msg_body)
+
     msg = MIMEMultipart("alternative")
-    msg["Subject"] = msg_body
+    msg["Subject"] = subject
     msg["From"]= EMAIL
     msg["To"] = recipient
 
+    html = '''
+       <html>
+       <body>
+       <h1>Estimado Usuario:''' + recipient + '''   </h1>
+       </body>
+       </html>
+       '''
+    asunto_text = "hola"
+    # creando elemento MIMEText
+    text_mime = MIMEText(asunto_text, 'plain')
+    html_mime = MIMEText(html, 'html')
 
+    # adjunta los MIMEText al MIMEMultipart
+    msg.attach(text_mime)
+    msg.attach(html_mime)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com") as smtp:
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", smtp_port, context=context) as smtp:
         smtp.login(EMAIL, PASSWORD)
-        smtp.sendmail(EMAIL, recipient, msg.as_string())
+        smtp.sendmail(EMAIL, recipient, msg=msg.as_string())
+    
     return jsonify('"ok: correo Enviado"'), 200
